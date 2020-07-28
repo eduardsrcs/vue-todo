22c54e64be8a9d01dcf9a672a7df5266e5913fe7
EduardsZ <eduards.zvaigzne@gmail.com>
2020-07-28 11:14:41 +0300

    initial commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,70 @@
+<template>
+<li>
+  <span 
+    :class="{done: todo.completed,
+    false: !todo.completed}"
+  >
+    <input
+      type="checkbox"
+      :checked="todo.completed"
+      @change="todo.completed = !todo.completed"
+    />
+    <strong>{{index + 1}}</strong>
+    {{todo.title | uppercase}}
+  </span>
+  <button
+    class="rm"
+    @click="$emit('remove-todo', todo.id)"
+  >
+    &times;
+  </button>
+</li>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    },
+    index: {
+      type: Number
+    }
+  },
+  filters: {
+    uppercase(value) {
+      return value.toUpperCase()
+    }
+  }
+}
+</script>
+
+<style scoped>
+li {
+  border: 1px solid #ccc;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem 1rem;
+  margin-bottom: 1rem;
+}
+
+.rm {
+  color: #fff;
+  border-radius: 50%;
+  background-color: #f33;
+  font-weight: bold;
+  outline: none;
+}
+
+.done {
+  text-decoration: line-through;
+}
+
+input {
+  margin-right: 1rem;
+}
+
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nam unde, debitis quod ipsum iure ipsam exercitationem aliquid. Voluptates quod ratione corporis architecto consequatur voluptas autem fugiat sed, voluptatem iste?</p>
+    <router-link to="/todos">Todos</router-link>
+
+  </div>
+</template>
